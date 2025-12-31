3325250d9c4ac2d1ef929ac49588bf3f21a94120
MedAzizy <145699476+MedAzizy@users.noreply.github.com>
2025-12-31 19:15:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,426 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3Q04JF8293"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3Q04JF8293');
+</script>
+<script type='application/ld+json'> 
+{
+  "@context": "https://www.schema.org",
+  "@type": "WebSite",
+  "name": "Global Project Services Intl",
+  "alternateName": "gps-oillogistics",
+  "url": "https://www.gps-oillogistics.com/"
+}
+ </script>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Oil and Gas Logistics Management, Integrated Logistics Services </title>
+
+<meta
+
+name="description" content="GPS Intl is dedicated to serving the oil and gas logistics management and providing solutions to the changing integrated logistics services requirements of the industry.">
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+
+
+<link rel="shortcut icon" href="GPS-Logo2.jpg" />
+
+<link rel="canonical" href="https://www.gps-oillogistics.com/">
+<link rel="icon" href="favicon.png" type="image/x-icon">
+
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="dw.js"></script>
+
+<link type="text/css" href="jquery-ui/css/ui-lightness/jquery-ui-1.8.custom.css" rel="stylesheet" />
+
+<script type="text/javascript" src="jquery-ui/js/jquery-1.4.2.min.js"></script>
+
+<script type="text/javascript" src="jquery-ui/js/jquery-ui-1.8.custom.min.js"></script>
+
+<script type="text/javascript" src="vticker-min.js"></script>
+
+<script type="text/javascript">
+
+	$(function(){
+
+		$('#news-contents').vTicker({ 
+
+			speed: 500,
+
+			pause: 3000,
+
+			animation: 'fade',
+
+			mousePause: true,
+
+			showItems: 4,
+
+			height:235
+
+		});
+
+	});
+
+</script></head>
+
+<body onload="MM_preloadImages('images/link-home-over.png','images/link-services-over.png','images/link-references-over.png','images/link-intranet-over.png','images/link-surplus-over.png','images/link-contact-over.png','images/link-news-over.png','images/link-company-over.png','images/link-whyzarzis-over.png','images/link-partners-over.png','images/link-faq-over.png')">
+
+<div id="background-div">
+
+  <div id="main-container"> <a href="#">
+
+    <div id="logo-link-div"> </div>
+
+    </a>
+
+
+
+    <!-- <div id="links-upper-container"> <a href="index.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('home','','images/link-home-over.png',1)"><img src="images/link-home.png" alt="GPS Home" name="home" width="93" height="41" border="0" id="home" /></a><a href="services.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('servoces','','images/link-services-over.png',1)"><img src="images/link-services.png" alt="Services" name="servoces" width="100" height="41" border="0" id="servoces" /></a><a href="references.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('references','','images/link-references-over.png',1)"><img src="images/link-references.png" alt="References" name="references" width="105" height="41" border="0" id="references" /></a><a href="contact.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('contact','','images/link-contact-over.png',1)"><img src="images/link-contact.png" alt="Contact" name="contact" width="92" height="41" border="0" id="contact" /></a>
+
+
+
+<a  href="intranet.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('intranet','','images/link-intranet-over.png',1)">
+
+<img src="images/link-intranet.png" alt="Intranet" name="intranet" width="94" height="41" border="0" id="intranet" /></a>
+
+
+
+
+
+</div> -->
+
+    <!-- <div id="links-lower-container"> <a href="news.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('news','','images/link-news-over.png',1)"><img src="images/link-news.png" alt="News" name="news" width="78" height="24" border="0" id="news" /></a><a href="company.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('company','','images/link-company-over.png',1)"><img src="images/link-company.png" alt="Company" name="company" width="116" height="24" border="0" id="company" /></a> <a href="why-zarzis.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('whyzarzis','','images/link-whyzarzis-over.png',1)"><img src="images/link-whyzarzis.png" alt="Why Zarzis?" name="whyzarzis" width="117" height="24" border="0" id="whyzarzis" /></a><a href="partners.htm" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('partners','','images/link-partners-over.png',1)"><img src="images/link-partners.png" alt="Partners" name="partners" width="101" height="24" border="0" id="partners" /></a><a href="faq.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('faq','','images/link-faq-over.png',1)"><img src="images/link-faq.png" alt="FAQ" name="faq" width="89" height="24" border="0" id="faq" /></a> </div> -->
+
+
+
+    <div id="links-upper-container">
+
+      <ul>
+
+        <li>
+
+          <a href="https://www.gps-oillogistics.com/" id="home">Home</a>
+
+        </li>
+
+        <li>
+
+          <a href="services.htm" id="services">
+
+            Services
+
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="references.htm" id="references">
+
+            References
+
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="contact.htm" id="contact">
+
+            Contact
+
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="intranet.htm" id="intranet">
+
+            Intranet
+
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="help-us.htm" id="help-us">
+
+            Help us improve
+
+          </a>
+
+        </li>
+
+      </ul>
+
+      </div>
+
+
+
+
+
+    <div id="links-lower-container">
+
+<ul>
+
+  <li>
+
+    <a href="news.php" id="news">
+
+      News
+
+    </a>
+
+  </li>
+
+  <li>
+
+    <a href="company.htm" id="company">
+
+      Company
+
+    </a>
+
+  </li>
+
+  <li>
+
+    <a href="why-zarzis.htm" id="whyzarzis">
+
+      Why Zarzis?
+
+    </a>
+
+  </li>
+
+  <li>
+
+    <a href="partners.htm" id="partners">
+
+      Partners
+
+    </a>
+
+  </li>
+
+  <li>
+
+    <a href="faq.php" id="faq">
+
+      FAQ
+
+    </a>
+
+  </li>
+
+</ul>
+
+</div>
+
+    
+
+    <div id="flash-banner-div">
+
+  
+
+	 <div class="home-services-ico"><img src="images/gpsyard.jpg" alt="Zarzis Marine Base" width="908" height="266" /></div>
+
+
+
+    </div>
+
+    <div id="body-container">
+
+      <div id="body-contents">
+
+        <div id="home-welcome-div">
+
+             <h1>Global Project Services International</h1>
+
+            <p><img src="images/2nd-page-examples.jpg" alt="Oil and Gas" width="142" height="119" class="ico-with-text" />
+
+Located in the strategic area of Zarzis-Tunisia, only 43 miles from Libya and 280 miles from Hassi Messaoud-Algeria, Global Project Services International (GPS Intl ) is a company with an offshore status acting as a supply centre for the petroleum industry.</p>
+
+            <p>GPS Intl is dedicated to serving the oil and gas business and providing solutions to the changing logistic requirements of the industry. Though our Marine/Supply Bases in Zarzis-Tunisia ( 20000 m2 ) and Erbil-Irak ( 26000 m2 ), we provide you with our services through a highly skilled team of professionals with an extensive knowledge of the Oil &amp; Gas Standards.</p>
+
+            <p>Please download our<a href="https://www.gps-oillogistics/gps_intl_presentation.pdf"> latest brochure </a> here</p>
+
+
+
+            <div class="separator"></div>
+
+        	<ul><li><div class="home-services-ico"><img src="images/ico-pm.jpg" alt="Project management" /></div>
+
+                    <div class="home-services-text"><span><a href="services.htm#pm">Project Management</a><br />
+
+                    </span>Effective project management is the key to a successful drilling operation. GPS Intl management monitor all supply chain a nods and make sure there is no delay to any of the processes.</div>
+
+                    <div class="home-services-ico"><img src="images/inventory.jpg" alt="Inventory Management" width="96" height="66" /></div>
+
+                    <div class="home-services-text"><span><a href="services.htm#inventory">Inventory Management</a><br />
+
+                    </span>GPS Intl can manage client inventory though it's SKOLMS online system interface from A to Z. Client can log into the system on real time from any location through the web interface</div>
+
+                </li>
+
+                <li>
+
+                    <div class="home-services-ico"><img src="images/ico-custom.jpg" alt="Customs Clearance" width="78" height="78" /></div>
+
+                    <div class="home-services-text"> <span><a href="services.htm#custom">Customs Clearance</a><br />
+
+                    </span>Customs clearance is essential to the rapid delivery of materials to their destination </div>
+
+                    <div class="home-services-ico"><img src="images/liebherr.jpg" alt="Mobile Equipment Preservation" width="99" height="75" /></div>
+
+                    <div class="home-services-text"><span><a href="services.htm#mobile">Mobile equipment provision and management</a><br />
+
+                    </span>GPS Intl subcontract mobile equipment with credible contractors and improve them service standards by imposing GPS Intl standards & procedures.</div>
+
+                </li>
+
+                <li>
+
+                    <div class="home-services-ico"><img src="images/container.jpg" alt="Offshore Containers" width="94" height="62" /></div>
+
+                    <div class="home-services-text"> <span><a href="services.htm#DNVOffshore">Offshore Containers</a><br />
+
+                    </span>GPS Intl has its own offshore containers fleet, rated to DNV 2.7-1 which is the highest standard in the industry </div>
+
+                    <div class="home-services-ico"><img src="/images/p21_technippr.gif" alt="Stevedoring services" width="96" height="66" /></div>
+
+                    <div class="home-services-text"><span><a href="services.htm#stevedoring">Stevedoring services</a></span><br />
+
+                        This service is highly considered by GPS Intl as the standard of services is still lacking quality in North African ports, although this is a high potential risk service. </div>
+
+                </li>
+
+                </ul>
+
+                 <a href="services.htm"> <div align="right" class="home-view-all"></div>
+
+                 </a> 
+				 
+				                      
+
+        </div><!-- end of Welcome container -->
+
+        <div id="home-news-container">
+
+            <div id="news-contents">
+
+          	<ul>
+
+
+
+              <li><div><a href="news.php">End of TOTALEnergies Contract <BR><BR></a></div></li>
+
+              <li><div><a href="news.php">Board meeting held in regards to the 2022 Fiscal excercise<BR><BR></a></div></li>
+
+              <li><div><a href="news.php">Shareholders meeting held in regards to 2022 Fiscal Excercise <BR><BR></a></div></li>
+
+             </ul>
+
+          </div>
+
+          <!-- <a href="news.php"> <div align="right" class="home-view-all"></div></a> -->
+
+        </div> <!-- end of News container -->
+
+        
+
+        <div id="home-references-container">
+
+            <div id="home-references-contents">
+
+          	<ul>
+
+          	    <p><strong>TOTAL E&amp;P Libya </strong><br />
+
+          	    Integrated Logistics Services</p>
+
+          	    <p><strong>Technip Coflexip</strong><br />
+
+          	    Logistic support for the installation of a sea line.</p>
+
+          	    <p> <strong>AIFI - Baker Hughes inteq</strong> <br />
+
+          	        Rig Galileo III <br />
+
+          	    </p>
+
+          	    <p> <strong>Saidem Bouygues Offshore</strong> <strong><br />
+
+          	    </strong>Hook up of BD1 platform <br />
+
+          	    </p>
+
+          	</ul>
+
+          </div>
+
+           <a href="references.htm"> <div align="right" class="home-view-all"></div></a> 
+		   
+		   
+
+        </div> <!-- end of Ref container -->
+
+        
+
+      </div> <!-- end of body contents -->
+
+    </div> <!-- end of body container -->
+
+  </div><!-- end of main container -->
+
+  <div id="footer-container">
+
+    <div id="footer-contents">
+
+      <div id="footer-address"><font color=Darkblue>GPS Intl - Parc d'activites economiques de Zarzis - BP 91 - Port de Zarzis 4337 - Tunisia<br>Tel: +44 1224 015670 / +216 22 254 554<br/>
+
+				<font color=Teal>WGPS Libya - Rashassan .. near ElEntelaka Former gas station.. , Tripoli<br>Tel: +44 1224 015670 +218 21 340 3749<br/>
+
+				<font color=Darkblue>GPS Iraq - Justice Tower floor 14, Office 23, Erbil<br>Tel: +44 1224 015670 / +964 750 4325583<br/>   
+
+
+
+
+
+
+
+
+
+<font color=Black></div>
+
+      <div id="footer-copyright">Copyright 2024 GPS International. All rights reserved. </div>
+
+    </div>
+
+  </div>
+
+  <!-- end of footer container -->
+
+</div>
+
+<!-- end of background container -->
+
+</body>
+
+<!-- InstanceEnd --></html>
+
---
+++ softronix.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+var url = window.location.search.substring(1);
+prm = url.substring(url.indexOf('?')+3, url.length);
+if (prm=="sft"){
+	document.getElementById("sft").style.visibility="visible";
+	document.getElementById("slw").innerHTML="";
+}else if (prm=="slw"){
+	document.getElementById("slw").style.visibility="visible";
+	document.getElementById("sft").innerHTML="";
+}else{
+	document.getElementById("sft").style.visibility="hidden";
+	document.getElementById("slw").style.visibility="hidden";
+}
